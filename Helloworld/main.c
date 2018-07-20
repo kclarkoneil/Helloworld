d1f6170f443a79e0a74c5d17c9b148daf831ecf3
HighGroundLover <kclarkoneil@yahoo.ca>
2018-07-20 13:18:05 -0700

    Initial Commit

---
+++ Helloworld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Helloworld
+//
+//  Created by Kit Clark-O'Neil on 2018-07-20.
+//  Copyright © 2018 Kit Clark-O'Neil. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
